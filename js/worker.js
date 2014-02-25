0859c40d097d9df4991ef2e76970fcd7f884c65a
trembl <g@trembl.org>
2014-02-25 10:49:02 +0900

    Removing dead cells

--- js/worker.js
+++ js/worker.js
@@ -9,9 +9,63 @@ if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScop
 	var settings;
 	
 } else {
+	// debugging
+	// Single Inital Cell Object
 	
-	var cells = [{"index":0,"radius":12,"children":[],"parent":false,"alive":true,"born":0,"randomAngle":4.663045245111483,"color":"#e41a1c","x":429.8238424033283,"y":254.25476253409252,"driverGenes":[false,false,false,false,false,false,false,false,false,false],"genes":[false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],"weight":0,"px":429.822615756692,"py":254.25461963429507},{"index":1,"radius":13,"children":[],"parent":false,"alive":true,"born":0,"randomAngle":5.382316351423005,"color":"#377eb8","x":390.37445825665606,"y":261.87912490241206,"driverGenes":[false,false,false,false,false,false,false,false,false,false],"genes":[false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],"weight":0,"px":390.3772615597312,"py":261.8812832047626},{"index":2,"radius":11,"children":[],"parent":false,"alive":true,"born":0,"randomAngle":4.644964903055521,"color":"#4daf4a","x":407.6315790519284,"y":221.18797463911895,"driverGenes":[false,false,false,false,false,false,false,false,false,false],"genes":[false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],"weight":0,"px":407.6316551441446,"py":221.18661364874498},{"index":3,"radius":11,"children":[],"parent":false,"alive":true,"born":0,"randomAngle":3.7686710180349916,"color":"#984ea3","x":388.498201940526,"y":232.0755569600501,"driverGenes":[false,false,false,false,false,false,false,false,false,false],"genes":[false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],"weight":0,"px":388.49790433024526,"py":232.07383387214583},{"index":4,"radius":11,"children":[],"parent":false,"alive":true,"born":0,"randomAngle":3.714136003849895,"color":"#ff7f00","x":427.1400690470029,"y":231.39449804354894,"driverGenes":[false,false,false,false,false,false,false,false,false,false],"genes":[false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],"weight":0,"px":427.13985868832185,"py":231.39405969820862},{"index":5,"radius":12,"children":[],"parent":false,"alive":true,"born":0,"randomAngle":6.275775150196805,"color":"#ffff33","x":408.0355406404851,"y":244.19146753239022,"driverGenes":[false,false,false,false,false,false,false,false,false,false],"genes":[false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],"weight":0,"px":408.03483896796547,"py":244.1901880753529}];
-	var settings = {"n":100,"p0":0.001,"q0":1e-7,"r":0.01,"d":10,"maxNumberOfCells":100000,"initialCells":6};
+	var settings = {
+		n: 20,						// total number of genes, including driver (d) and essential (e) genes
+		r: 0.01, 					// mutation probabiliy of each gene when replicated
+		d: 10, 						// number of driver genes, first d genes of g
+		e: 5,						// number of essential genes, number of genes
+		mutationRate: 0.01,
+		growthRate: 0.01,			// 0.0001
+		deathRate: 0.001,		
+		deathRateForNonStem: 0.001,
+		srp: 0.5,	// between 0 and 1
+		fitnessIncrease:5, 
+		maxNumberOfCells: 1000,
+		initialCells: 6,
+	};
+	
+	cells = [];
+	
+	var initGenes = function(nr) {
+		var genes = [];
+		for(var i=0; i<nr; i++) {
+			genes.push(false);
+		}
+		return genes;	
+	}
+	var StemCell = function() {
+		var i = cells.length;
+		this.index = i;				// append to cells array
+		this.isStem = true;
+		this.subtype = i;			// initial subtypes
+		this.radius = 0;			// important to set initial radius to 0
+		this.finalRadius = Math.floor(Math.random() * 5 + 10);
+		this.children = [];
+		this.parent = false;	// root, no parents
+		this.alive = true;
+		this.died = undefined;						// timestep
+		this.born = 0;								// timestep, start, 0
+		this.randomAngle = Math.random()*Math.PI*2;
+		this.color = "#44aaCC";
+		this.x = 0;
+		this.y = 0;
+		this.driverGenes = initGenes(settings.d);			// false -> not mutated
+		this.essentialGenes = initGenes(settings.e);
+		this.genes = initGenes(settings.n - settings.e - settings.d);		// false -> not mutated
+		return this;
+	}
+
+	var initCells = function(nr) {
+		cells = [];
+		for(var i=0; i<nr; i++) {
+			cells.push(new StemCell());
+		}
+	}
+
+	initCells(5);
 }
 
 // return list of dead cells
@@ -20,33 +74,33 @@ if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScop
 var time = 0;			// global simulation time
 var running = false;	// running status
 
-
+var info = {};
 
 var intervalID;
+
 onmessage = function(e) {
 	console.log("worker receiving message");
 	var data = JSON.parse(e.data);
 	
 	console.log(data.message);
 	
-	if (data.settings) {
+	if (data.settings) {			// update settings..
 		settings = data.settings;
+		console.log("settings updated");
 	}
 	
-	if (data.cells) {
-		cells = data.cells;
+	if (data.cells) {				// update cells..
+		cells = data.cells;	
 	}
 	
 	if (data.message === "startSimulation") {
 		console.log("worker: startSimulation");
 		running = true;
-		// update settings..
-		simulate(1000);	
-		postMessage( JSON.stringify({'message':'firstCells', 'cells':cells}) );
+		
+		simulate(settings.maxNumberOfCells);
+		updateInfo();
+		postMessage( JSON.stringify({'message':'simulationFinished', 'cells':cells, 'info':info}) );
 	
-		simulate(100000);		// simulate rest
-		postMessage( JSON.stringify({'message':'allCells', 'cells':cells}) );
-	//	postMessage( JSON.stringify({'message':'simulationFinished'}) );
 	}
 	
 	
@@ -55,15 +109,18 @@ onmessage = function(e) {
 
 
 
+var cellRadius = function() {
+	return 12;
+//	return 10 + Math.floor(Math.random() * 10);
+}
 
-
-
-// Single Cell Object
+// Create a child Cell object from a parent Cell object
 var Cell = function(parent) {
 	this.index = cells.length;				// append to cells array
+	this.isStem = parent.isStem;				// stem or normal 
 	this.subtype = parent.subtype;			// propagate subtype
 	this.radius = 0;
-	this.finalRadius = Math.floor(Math.random() * 5 + 10);
+	this.finalRadius = cellRadius();		// randomize radius?
 	this.children = [];
 	this.alive = true;
 	this.died = undefined;				// timestep
@@ -71,26 +128,10 @@ var Cell = function(parent) {
 	this.randomAngle = Math.random()*Math.PI*2;
 	this.parent = parent.index;			// add parent id
 	parent.children.push(this.index);	// add child id to parent
-	
-	// Mutate Genese
 	this.genes = parent.genes.slice(0);					// copy genes
-	for (var j=0; j<this.genes.length; j++) {
-		if (!this.genes[j]) {							// 'gene' not mutated
-			if (Math.random() < settings.r) {			// mutation probabiliy of each gene when replicated
-				this.genes[j] = true;					// mutated
-			}
-		}
-	}
-	
-	// Mutate Driver Genes
 	this.driverGenes = parent.driverGenes.slice(0);		// copy parent driver genes
-	for (var k=0; k<this.driverGenes.length; k++) {
-		if (!this.driverGenes[k]) {						// gene' not mutated
-			if (Math.random() < settings.r) {			// mutation probabiliy of each gene when replicated
-				this.driverGenes[k] = true;				// mutated
-			}
-		}
-	}
+	this.essentialGenes = parent.essentialGenes.slice(0);		// copy parent driver genes
+	mutate(this);
 }
 
 
@@ -102,46 +143,193 @@ var Cell = function(parent) {
 // Simulate one generation
 
 var simulate = function(limit) {
+	console.time("simulation");
+	
 	if (cells.length===0) {
 		console.log("No cells defined.");
+		return;
 	}
 	
 	if (limit===undefined) {
 		limit = settings.maxNumberOfCells;
 	}
+	
+	
+	simulationLoop: 		// while loop label
 	while(true) {
 		var cell, i;
-		var length = cells.length;	
+		var length = cells.length;		// get new length
 
 		for (i=0; i<length; i++) {		// loop over cells
+			
+			if (cells.length === limit) {							//  break when the desired number of cells are reached
+				break simulationLoop;
+			}
+						
 			cell = cells[i];
-
-			if (Math.random() < settings.q0) {						// die
-				// update alive status
-				// time of death
-				// remove from display
+			
+			if (!cell.alive) continue;	// skip if cell is dead
+			
+			// Remove
+		
+			var dr = Math.random();
+			var drns = Math.random();
+		
+				
+			if ((dr < settings.deathRate) || (drns < settings.deathRateForNonStem)) {				// Death Rate
+				if (cell.index < 5) {
+					console.log("StemCell died " + cell.index);
+				}
+				cell.alive = false; 	// update alive status
+				cell.died = time;		// time of death
+				if (info.aliveCells === 0) break simulationLoop;		// no more alive cells left...
 				continue;
 			}
-
-			if (Math.random() < settings.p0) {						// replicate
-				var parent = cells[i];
-				var child = new Cell(parent);
-				cells.push(child);
+			
+			// Mutate & Replicate
+			if (Math.random() < (getFitness(cell) * settings.growthRate)) {
+				// mutate(cell); mutationg happens in new Cell()		
+				var childCell = new Cell(cell);		// copy cell
+				
+				// if cell is stem cell and random() is larger than srp, create a normal cell
+				if (cell.isStem && (Math.random() > settings.srp)) {
+					childCell.isStem = false;		// -> normal cell
+				}
+				
+				if (getFitness(childCell) < 0) {
+					childCell.alive = false; 	// update alive status
+					childCell.died = time;		// time of death
+			
+				} else {
+					// stem cells and normal cell have the same genome!? only difference is the isStem bit
+					cells.push(childCell);	
+				}
+				
 			}		
 			
-			if (cells.length === limit) {					//  break when the desired number of cells are reached
-				return;
-			}
+		
 			
 		}
-		time++;			// advance time
-		
+		time++;			// advance time on tick
 	}
+	
+	updateInfo();
+	console.timeEnd("simulation");
+}
+
+
+var getFitness = function(cell) {
+	var fitness = 1;
+	for (var k=0; k<cell.driverGenes.length; k++) {
+		if (cell.driverGenes[k]) {						// gene not already mutated
+			fitness *= settings.fitnessIncrease;
+		}
+	}
+	
+	for (var j=0; j<cell.essentialGenes.length; j++) {
+		if (cell.essentialGenes[j]) {							// gene not already mutated
+			fitness = -1;										// if fitness is <0, cell dies
+			break;
+		}
+	}
+	return fitness;
+}
+
+
+var mutate = function(cell) {
+	if (cell.alive === false) return;		// don't mutate if cell is dead
+	mutateGenes(cell.driverGenes);
+	mutateGenes(cell.essentialGenes);
+	mutateGenes(cell.genes);
+}
+
+var mutateGenes = function(genes) {
+	for (var i=0; i<genes.length; i++) {
+		if (genes[i]===false) {
+			if (Math.random() < settings.mutationRate) {	// not mutated, & mutation probabiliy of each gene when replicated
+				genes[i] = time;												// change to mutated time
+			} 
+		}
+	}	
+}
+
+
+
+var updateInfo = function() {
+	console.time("updateInfo");
+	var allCells = 0,
+	aliveCells = 0,
+	deadCells = 0,
+	stemCells = 0,
+	normalCells = 0,
+	links = [];
+	
+	for (var i=0; i<cells.length; i++) {
+		var cell = cells[i];
+		if (cell.alive) {
+			aliveCells++;	
+		} else {
+			deadCells++;
+		}
+		if (cell.isStem) stemCells++;
+		if (!cell.isStem) normalCells++;
+		
+		// create Links
+		if (cell.parent) {		// original cells don't have parents
+			links.push({'source': cell.parent, 'target': cell.index});
+		}
+	}
+	
+	info.allCells = cells.length;
+	info.aliveCells = aliveCells;
+	info.deadCells = deadCells;
+	info.stemCells = stemCells;
+	info.normalCells = normalCells;
+	info.links = links;
+	
+	compareGenes();
+	console.timeEnd("updateInfo");
 }
 
 
 
 
+
+var compareGenes = function() {
+	console.time("compareGenes");
+	
+	var genesToBinary = function(geneArray) {
+		var str = geneArray.map(function(value, i, arr) {
+			if (value) return 1;
+			return 0;
+		});
+		str = str.join("");
+		return str;
+	}
+	
+	// convert mutation arrays to binary strings
+	genes = cells.map(function(cell, i) {
+		var driverGenes = genesToBinary(cell.driverGenes);
+		var essentialGenes = genesToBinary(cell.essentialGenes);
+		var genes = genesToBinary(cell.genes);
+		return driverGenes + essentialGenes + genes;
+	});
+
+	// get unique genes
+	var reducedGenes = {};
+	for (var i=0; i<genes.length; i++) {
+		var gene = genes[i];
+		if (reducedGenes[gene] === undefined) {
+			reducedGenes[gene] = [i];
+		} else {
+			reducedGenes[gene].push(i);
+		}
+	}
+	info.reducedGenes = reducedGenes;
+	
+	
+	console.timeEnd("compareGenes");
+}
 
 
 
