105cd39cc589bc344ff795fb23077d162b338e71
trembl <g@trembl.org>
2014-04-25 16:59:51 +0900

    Summarising genes

--- js/worker.js
+++ js/worker.js
@@ -15,7 +15,37 @@ var settings = {
 	initialCells: 6,
 };
 
-var cells = [];
+var cells = [];  // Create a nested structure for cells
+
+/*
+
+{
+ "name": "flare",
+ "children": [
+  {
+   "name": "analytics",
+   "children": [
+    {
+     "name": "cluster",
+     "children": [
+      {"name": "AgglomerativeCluster", "size": 3938},
+      {"name": "CommunityStructure", "size": 3812},
+      {"name": "MergeEdge", "size": 743}
+     ]
+    },
+    {
+     "name": "graph",
+     "children": [
+      {"name": "BetweennessCentrality", "size": 3534},
+      {"name": "LinkDistance", "size": 5731}
+     ]
+    }
+   ]
+  }
+ ]
+}
+
+*/
 
 if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope) {
 	importScripts('science.v1.min.js');
